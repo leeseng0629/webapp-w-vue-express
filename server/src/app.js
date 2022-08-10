d3af0b17f5d560ccbf346b6148b85d6bf7707fa6
leeseng0629 <tekken_teoh95@hotmail.com>
2022-08-10 16:41:22 +0800

    register module front end done

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,22 @@
-const express = require("express");
+const express = require('express');
 // const bodyParser = require("body-parser");
-const cors = require("cors");
-const morgan = require("morgan");
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(cors());
 
-app.get("/status", (req, res) => {
-  res.json({
-    msg: "Hello world",
+app.post('/api/v1/register', (req, res) => {
+  res.send({
+    msg: `Hello ${req.body.email}, you have been registered, Have fun. `,
   });
 });
 
-app.listen(process.env.PORT || 8081);
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
